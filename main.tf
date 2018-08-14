2a3266423d0dc8021cd6b7a21f6122237deac1d7
Atanas Chuchulev <achuchulev@Atanass-MacBook-Pro.local>
2018-08-14 14:16:44 +0300

    run fmt

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "null_resource" "helloWorld" {
     command = "echo hello word"
   }
 }
+
 resource "null_resource" "helloWorld1" {
   provisioner "local-exec" {
     command = "echo hello word1"
