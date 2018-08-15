3adcc50f2d27f608f11cd76806120fefed62ffb5
achuchulev <42144722+achuchulev@users.noreply.github.com>
2018-08-15 10:44:20 +0300

    add another null resource

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# We use this null_provider to simulate terraform real exec
 resource "null_resource" "helloWorld" {
   provisioner "local-exec" {
     command = "echo hello word"
@@ -7,5 +8,11 @@ resource "null_resource" "helloWorld" {
 resource "null_resource" "helloWorld1" {
   provisioner "local-exec" {
     command = "echo hello word1"
+  }
+}
+
+resource "null_resource" "helloWorld2" {
+  provisioner "local-exec" {
+    command = "echo hello word2"
   }
 }
